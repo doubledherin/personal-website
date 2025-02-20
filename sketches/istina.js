5a861a7818be74531657f63c2778cf44afea0f7e
Wendy Dherin <doubledherin@users.noreply.github.com>
2025-02-19 19:33:31 -0800

    First project.

---
+++ sketches/istina.js
@@ -0,0 +1,52 @@
+let diameter
+let angle = 0
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+  diameter = width / 4
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  stroke(255, 255, 204)
+  diameter = width / 4
+  strokeWeight(1.8)
+}
+
+// function keyPressed() {
+//   if (key === "s") {
+//     saveGif("mySketch", 5)
+//   }
+// }
+
+function draw() {
+  // console.log(frameCount % 360)
+  background(10, 15, 40)
+  drawingContext.shadowBlur = 50
+  drawingContext.shadowColor = color(255, 255, 204)
+  translate(width / 2, height / 2)
+
+  // TO DO: make increment based on mouse position distance from center
+  // where center is most complex and edges and beyond is least complex
+  // or base it on sound! make the person play a note or hum a note
+  const increment = floor(map(cos(angle), -1, 1, 15, 181))
+  for (let a = 0; a <= 360; a += increment) {
+    push()
+    rotate(radians(a))
+    for (let radianValue = 0; radianValue <= 180; radianValue += 10) {
+      // mess with 8
+      line(
+        sin(radians(radianValue)) * diameter,
+        cos(radians(radianValue)) * diameter,
+        sin(radians(-radianValue)) * diameter,
+        cos(radians(-radianValue)) * diameter
+      )
+    }
+    pop()
+  }
+  noFill()
+  push()
+  strokeWeight(1.2)
+  ellipse(0, 0, diameter * 2, diameter * 2)
+  pop()
+  angle += 0.013
+}
